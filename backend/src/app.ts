8c0b685f53ae26f9116426c3a276f088ef8305bc
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-07 22:36:20 -0600

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,39 +1,26 @@
-// import express from 'express';
-// import { config } from 'dotenv';
-// import morgan from 'morgan';
-// import cookieParser from 'cookie-parser';
-// import userRoutes from './routes/userRoutes.js';
-// import chatRoutes from './routes/chatRoutes.js';
-// import cors from 'cors';
-// import { connectDB } from './db/connect';
+import express from 'express';
+import { config } from 'dotenv';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
+import chatRoutes from './routes/chatRoutes.js';
+import cors from 'cors';
 
-// config();
-// const app = express();
-// const port = process.env.PORT || 4000;
+config();
+const app = express();
 
-// // Middleware
-// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-// app.use(express.json());
-// app.use(cookieParser(process.env.COOKIE_SECRET));
-// app.use(morgan('dev')); // for development
+// Middleware
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan('dev')); // for development
 
-// // Routes
-// app.use('/api/user', userRoutes);
-// app.use('/api/chat', chatRoutes);
+// Routes
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 
-// app.get('/', (req, res, next) => {
-//     res.send('Api is running...');
-// });
+app.get('/', (req, res, next) => {
+    res.send('Api is running...');
+});
 
-// // Connection
-// const start = async () => {
-//     try {
-//         await connectDB(process.env.MONGO_URI);
-//         app.listen(port, () => console.log(`server listening on port ${port}`));
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-// start();
-
-// export default app;
+export default app;
