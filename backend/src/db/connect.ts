171f2b6e1879bfa630471d8c406c83a35da2624c
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-08 16:03:52 -0600

    update

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const connectDB = async (url: string) => {
+const connectDB = async (url) => {
   try {
     const conn = await mongoose.connect(url);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
