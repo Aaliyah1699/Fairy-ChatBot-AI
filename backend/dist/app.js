31b1708c0c02715affc4c569903604a9aed006e4
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-02-07 22:36:54 -0600

    update

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,36 +1,27 @@
 "use strict";
-// import express from 'express';
-// import { config } from 'dotenv';
-// import morgan from 'morgan';
-// import cookieParser from 'cookie-parser';
-// import userRoutes from './routes/userRoutes.js';
-// import chatRoutes from './routes/chatRoutes.js';
-// import cors from 'cors';
-// import { connectDB } from './db/connect';
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// config();
-// const app = express();
-// const port = process.env.PORT || 4000;
-// // Middleware
-// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-// app.use(express.json());
-// app.use(cookieParser(process.env.COOKIE_SECRET));
-// app.use(morgan('dev')); // for development
-// // Routes
-// app.use('/api/user', userRoutes);
-// app.use('/api/chat', chatRoutes);
-// app.get('/', (req, res, next) => {
-//     res.send('Api is running...');
-// });
-// // Connection
-// const start = async () => {
-//     try {
-//         await connectDB(process.env.MONGO_URI);
-//         app.listen(port, () => console.log(`server listening on port ${port}`));
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-// start();
-// export default app;
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = require("dotenv");
+const morgan_1 = __importDefault(require("morgan"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const userRoutes_js_1 = __importDefault(require("./routes/userRoutes.js"));
+const chatRoutes_js_1 = __importDefault(require("./routes/chatRoutes.js"));
+const cors_1 = __importDefault(require("cors"));
+(0, dotenv_1.config)();
+const app = (0, express_1.default)();
+// Middleware
+app.use((0, cors_1.default)({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)(process.env.COOKIE_SECRET));
+app.use((0, morgan_1.default)('dev')); // for development
+// Routes
+app.use('/api/user', userRoutes_js_1.default);
+app.use('/api/chat', chatRoutes_js_1.default);
+app.get('/', (req, res, next) => {
+    res.send('Api is running...');
+});
+exports.default = app;
 //# sourceMappingURL=app.js.map
